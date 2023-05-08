2aa3944753e56f873f6dbaac04d98aaa627feef9
orellanaJavier <78707211+orellanaJavier@users.noreply.github.com>
2023-05-07 18:23:30 -0600

    aceptat¿r

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-
+import { Card } from 'react-native-paper';
 import { StyleSheet, View,Button,Image ,} from 'react-native';
 import  React from 'react';
 import { NavigationContainer, } from '@react-navigation/native';
@@ -7,30 +7,40 @@ import NailsBar from './screens/NailsBar';
 import StylingArea from './screens/StylingArea';
 import CabinaArea from './screens/CabinaArea';
 
+
 const Stack = createNativeStackNavigator();
 
 function HomeScreen({navigation}){
   return(
     <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
       <Image resizeMode='cover' source={require('./assets/homescreen.png')} style = {styleSheet.styleImage9}/>
-        <Button title='Nails Bar' style ={{}} onPress={()=> navigation.navigate('NailsBar')}>
-        </Button>
-        <Button title='Styling Area' style={{}} onPress={()=> navigation.navigate('StylingArea')}>
-        </Button>
-        <Button title='Cabina Area' style={{}} onPress={()=> navigation.navigate('CabinaArea')}>
-        </Button>
+      <Card><Button title='Nails Bar'  onPress={()=> navigation.navigate('NailsBar')}>
+        </Button></Card>
+        <Card><Button title='Styling Area' onPress={()=> navigation.navigate('StylingArea')}>
+          </Button></Card>
+          <Card><Button title='Cabina Area'  onPress={()=> navigation.navigate('CabinaArea')}>
+        </Button></Card>
+    </View>
+  );
+};
+
+function Registro({navigation}){
+  return(
+    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+      <Card><Button style={{po}} title='Aceptar' onPress={()=> navigation.navigate('Home')}></Button></Card>
     </View>
   );
 };
 
 function App() {
   return (
-    <NavigationContainer>
-    <Stack.Navigator>
+    <NavigationContainer >
+    <Stack.Navigator initialRouteName='Registro'>
     <Stack.Screen name="Home" component={HomeScreen} />
     <Stack.Screen name='NailsBar' component={NailsBar}/>
     <Stack.Screen name='StylingArea' component={StylingArea}/>
     <Stack.Screen name='CabinaArea' component={CabinaArea}/>
+    <Stack.Screen name='Registro' component={Registro}/>
     </Stack.Navigator>
     </NavigationContainer>
   );
@@ -47,5 +57,4 @@ const styleSheet = StyleSheet.create({
     flex:1,
     justifyContent:'center',
   },
- 
 });
