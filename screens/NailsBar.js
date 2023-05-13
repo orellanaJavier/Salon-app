e7eccc342754a8822463b9f37cafc65026cfc18b
Ehaviles7 <ehaviles7@gmail.com>
2023-05-12 19:58:31 -0600

    probando commit

--- screens/NailsBar.js
+++ screens/NailsBar.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import {View,StyleSheet,Button } from 'react-native';
+import {View,StyleSheet,Button,Text, ScrollView } from 'react-native';
+
 
  function NailsBar() {
-  <View>
+  return(
+    <View style = {styleSheet.container}>
+    <ScrollView contentContainerStyle = {{
+      flex:1,
+      width:'100%',
+      height:'100%'
+    }}></ScrollView>
 
   </View>
+  )
+  
 }
 
 export default NailsBar;
 
 const styleSheet = StyleSheet.create({
+    container:{
+      flex: 1,
+      backgroundColor: '#'
+
+
+    }  
 });
 
