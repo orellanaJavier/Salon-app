760d9bc858411e623dc981d01eca8e413e4f10c2
orellanaJavier <78707211+orellanaJavier@users.noreply.github.com>
2023-05-20 15:59:28 -0600

    sabado 20 de mayo

--- screens/StylingArea.js
+++ screens/StylingArea.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import { View,StyleSheet, Button,Text} from 'react-native';
+import React, { useState } from 'react'
+import { View,StyleSheet, Button,Text,TouchableOpacity,Modal} from 'react-native';
 import { ScrollView } from 'react-native';
 import { Card } from 'react-native-paper';
 
-
  function StylingArea  () {
+const [open, setOpen] = useState(false)
+
+function handleOnPress (){
+  setOpen(open);
+}
+
   return (
     <View style = {styleSheet.container}> 
     <ScrollView contentContainerStyle={{
       flex:1,
       width:'100%',
-      height:'100%',
+      height:'150%',
       alignItems:'center',
       justifyContent:'center'}}>
         <Card><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,}}>Decoloracion:</Text></Card>
@@ -22,6 +27,16 @@ import { Card } from 'react-native-paper';
       <Card><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Pistoleado</Text></Card>
       <Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Secado</Text>
       <Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Peinados</Text>
+      <TouchableOpacity onPress={handleOnPress}>
+        <Text>Agendar</Text>
+      </TouchableOpacity>
+      <Modal animationType='slide' transparent={true} visible={open}>
+        <View>
+          <View>
+            
+          </View>
+        </View>
+      </Modal>
     </ScrollView>
     </View>
   );
