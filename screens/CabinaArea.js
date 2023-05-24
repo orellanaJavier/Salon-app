84cf659b9fbc87e106d43df20aa4c07e9cf34573
Tavo-Carrillo <smts091022@gmail.com>
2023-05-24 09:50:43 -0600

    gus

--- screens/CabinaArea.js
+++ screens/CabinaArea.js
@@ -12,6 +12,7 @@ function CabinaArea() {
       <Card><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Tratamientos</Text></Card>
       <Card><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Planchado</Text></Card>
       <Card><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Pistoleado</Text></Card>
+      <Card><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Secado</Text></Card>
       
 
     </View>
