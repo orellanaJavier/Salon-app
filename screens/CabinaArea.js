e18c53f83ea85c5ec7a10b09b01f0ed020dee23c
Ehaviles7 <ehaviles7@gmail.com>
2023-05-25 16:26:29 -0600

    Yes

--- screens/CabinaArea.js
+++ screens/CabinaArea.js
@@ -1,6 +1,5 @@
 import  React from 'react';
-import { View, Text, Button, StyleSheet, TouchableOpacity, Modal} from 'react-native';
-import { ScrollView } from 'react-native';
+import { View, Text, StyleSheet} from 'react-native';
 import { Card } from 'react-native-paper';
 
 
