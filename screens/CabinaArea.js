b0ade5e3ae0daad5d51eead550936075d2913e56
Ehaviles7 <ehaviles7@gmail.com>
2023-05-25 11:22:05 -0600

    tygt

--- screens/CabinaArea.js
+++ screens/CabinaArea.js
@@ -7,7 +7,7 @@ import { Card } from 'react-native-paper';
 function CabinaArea() {
   return (
     <View style={styleSheet.container}>
-      <Card style={{top:-40}}><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Cortes de cabello</Text></Card>
+      <Card><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Cortes de cabello</Text></Card>
       <Card><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Alisados</Text></Card>
       <Card><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Tratamientos</Text></Card>
       <Card><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Planchado</Text></Card>
