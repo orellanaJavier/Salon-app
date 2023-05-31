492af9c24daef0eb63d872f8712a62bbc3be0acf
orellanaJavier <78707211+orellanaJavier@users.noreply.github.com>
2023-05-31 11:27:56 -0600

    agd

---
+++ navigation/Agenda.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { useState } from 'react';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+function Agenda()  {
+  return (
+    <View>
+      <Text>Agenda</Text>
+    </View>
+  )
+};
+
+export default Agenda;
+
+const styles = StyleSheet.create({})
