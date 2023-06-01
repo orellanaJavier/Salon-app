7d2a6cf2d054556883127c874f983ff47931c90b
orellanaJavier <78707211+orellanaJavier@users.noreply.github.com>
2023-06-01 11:31:44 -0600

    wer

--- screens/StylingArea.js
+++ screens/StylingArea.js
@@ -11,22 +11,25 @@ import { Card } from 'react-native-paper';
     <View style = {styleSheet.container}> 
     <ScrollView contentContainerStyle={{
       flex:1,
-      width:'170%',
-      height:'170%',
+      width:'220%',
+      height:'220%',
       alignItems:'center',
       justifyContent:'center'}}>
-        <Card style={{top:-40}}><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Decoloracion:</Text>
-        <Text></Text>
+        <Card style={{top:-60}}><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Decoloracion:</Text>
+        <Text>precio: $50</Text>
         </Card>
-        <Card><Text  style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Tinturacion: </Text></Card>
-      <Card><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Cortes de cabello</Text></Card>
-      <Card><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Alisados</Text></Card>
-      <Card><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Tratamientos</Text></Card>
+        <Card style={{top:-45}}><Text  style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Tinturacion: </Text></Card>
+      <Card style={{top:-30}}><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Corte de cabello</Text></Card>
+      <Card style={{top:-20}}><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Alisados</Text></Card>
+      <Card style={{top:-9}}><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Tratamientos</Text></Card>
       <Card><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Planchado</Text></Card>
       <Card><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Pistoleado</Text></Card>
-      <Card><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Secado</Text></Card>
-      <Card><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Peinados</Text></Card>
+      <Card style={{top:15}}><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Secado</Text></Card>
+      <Card style={{top:30}}><Text style={{textAlign:'center',fontWeight:'bold',fontSize:14,margin:14}}>Peinados</Text></Card>
+      <Card style={{top:45}}>
       <Button title='Agendar Cita' onPress={()=> navigation.navigate('Agenda')}/>
+      </Card>
+      
     </ScrollView>
       
     </View>
@@ -39,9 +42,12 @@ export default StylingArea;
 
 const styleSheet = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     backgroundColor:'#fff',
     alignItems:'center',
     justifyContent:'center',
+  },
+  buttonn:{
+    top:30
   },
 });
